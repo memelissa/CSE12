8d520edcb3b3a4a523e7be04493eaf131149345b
memelissa <m7phan@ucsd.edu>
2022-05-04 20:36:22 -0700

    lec 2

--- wkp1/Event.java
+++ wkp1/Event.java
@@ -1,33 +1,29 @@
-class Calendar { /*  */ }
+import java.time.LocalDateTime;
 
 public class Event {
+  LocalDateTime start;
+  LocalDateTime end;
+  String location;
 
-	// Time
-	
-	//Idea: represent date/time with an int
-	int datetime;
-	int secondsSinceJanFirst1970;	//long
-	
-	//Idea: represent date/time with a String
-	String dateTime;	//"Jan 7 2019" - look up standard format
-	
-	//Idea: represent time + duration as TWO TIMES - start/end time
-	String start;
-	String end;
-	
-	//Idea: represent time as a new class
-	DateTime startTime;		//java has a built one
-	DateTime endTime;
-	
-	// Duration of the event
-	int minutes;	
-	
-	// Location 
-	String location;
-	float latitude, longitude;
-	
-	Location location;	
-		
-	// Determine if two Events conflict
-	
+  public Event(LocalDateTime start, LocalDateTime end, String location) {
+    this.start = start;
+    this.end = end;
+    this.location = location;
+  }
+
+  // Return true if the other event is in 
+  // the same room at an overlapping time
+  public boolean conflict(Event other) {
+	  
+	  if (!location.equals(other.location)) { return false; }
+	  
+	  if ((this.start.compareTo(other.start) <= 0) &&
+	  	  (this.end.compareTo(other.start) > 0))
+	  {
+		  return true;
+	  }
+	  
+	  
+	  return false;
+  }
 }
